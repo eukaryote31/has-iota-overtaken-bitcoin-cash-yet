333b0f2f0769e1430cb5ed254575410fd6cd894a
eukaryote <eukaryote31@users.noreply.github.com>
2017-12-05 13:56:37 -0700

    Display whether rank is ahead

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-$(".content").text($.getJSON("https://api.coinmarketcap.com/v1/ticker/iota/"))
+let iotaticker = $.getJSON("https://api.coinmarketcap.com/v1/ticker/iota/")
+let bchticker = $.getJSON("https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/")
+
+if (parseInt(iotaticker.rank) < parseInt(bchticker.rank)) {
+  $(".content").text("Yes")
+} else {
+  $(".content").text("No")
+}
